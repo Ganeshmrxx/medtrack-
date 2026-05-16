909356e6c3b827a919f3085f3bc05b164d628b26
g3cwo <ganesh7503@gmail.com>
2026-05-16 23:04:38 +0530

    Forced Service Worker update to show new features

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'medtrack-v1';
+const CACHE_NAME = 'medtrack-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -9,6 +9,17 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
     );
 });
 
